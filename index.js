7cddc26b2141150997116995957c225c48bf7c89
Carlos Vera <cavuforex@gmail.com>
2025-05-14 16:08:16 -0600

    Habilitado CORS para frontend WordPress

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
